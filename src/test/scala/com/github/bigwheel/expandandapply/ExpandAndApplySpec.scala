3a4676d3351b5eea62a41fbf3a23384031cd49be
bigwheel <k.bigwheel+eng@gmail.com>
2018-11-21 23:44:29 +0900

    Add expandAndPipe method

--- src/test/scala/com/github/bigwheel/expandandapply/ExpandAndApplySpec.scala
+++ src/test/scala/com/github/bigwheel/expandandapply/ExpandAndApplySpec.scala
@@ -4,13 +4,19 @@ import org.scalatest._
 
 class ExpandAndApplySpec extends FunSpec with Matchers {
 
+  private[this] def itemDescription(name: String, yen: Int): String = s"$name: ${yen}yen"
+  private[this] case class Item(name: String, yen: Int)
+  private[this] val cola = Item("cola", 120)
+
   describe("expandAndApply") {
     it ("should works correctly") {
-      def itemDescription(name: String, yen: Int): String = s"$name: ${yen}yen"
-      case class Item(name: String, yen: Int)
-      val cola = Item("cola", 120)
-
       (itemDescription _).expandAndApply(cola) should be("cola: 120yen")
+    }
+  }
+
+  describe("expandAndPipe") {
+    it ("should works correctly") {
+      cola.expandAndPipe(itemDescription _) should be("cola: 120yen")
     }
   }
 
